be4f43d88be5e9c90b1151b456195c6fbcc5bdb1
dlskrud815 <dlskrud815@gmail.com>
2024-05-03 20:34:28 +0900

    Random 함수 간략화

--- 240503_cpp_practice2/240503_cpp_practice2.cpp
+++ 240503_cpp_practice2/240503_cpp_practice2.cpp
@@ -5,30 +5,27 @@
 
 using namespace std;
 
-int Random(int randomNumber, int randNum)
+int Random()
 {
-	srand(time(NULL));
-	randomNumber = rand();
-	randNum = randomNumber % 45 + 1; //1~45 범위 지정
-
-	return randNum;
+	int randomNumber = rand();
+	return randomNumber % 45 + 1;
 }
 
 int main()
 {
 	cout << "* 로또 번호 추천 프로그램 *" << endl;
 
-	int num = 6, randomNumber=0, randNum=0, temp1=0, temp2=0;
+	int num = 6, temp1=0, temp2=0;
 
 	for (int i = 0; i < num; i++)
 	{
-		temp1 = Random(randomNumber, randNum);
+		temp1 = Random();
 
 		while (1)
 		{
 			if (temp2 == temp1)
 			{
-				temp1 = Random(randomNumber, randNum);
+				temp1 = Random();
 			}
 			else
 			{
